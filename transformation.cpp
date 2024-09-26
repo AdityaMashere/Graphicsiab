ac8e02a395f5fa5e2c8b673492b7aeeafde853da
AdityaMashere <158165635+AdityaMashere@users.noreply.github.com>
2024-09-26 18:26:07 +0530

    Update transformation.cpp

--- transformation.cpp
+++ transformation.cpp
@@ -1,71 +1,111 @@
 #include <graphics.h> 
 #include <cmath>
-#include<iostream>
+#include <iostream>
 
 void translation()
-{ int gd = DETECT,gm,left=100,top=200,right=200,bottom=200,x= 150,y=150,radius=50,degrees=0;
-   initgraph(&gd,&gm,NULL);
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
 
-   rectangle(50,50,150,120);
-   delay(2000);
-   cleardevice();
-   int constant = 200;
-   rectangle(50+constant,50+constant,150+constant,120+constant);
+    rectangle(50, 50, 150, 120);
+    delay(2000);
+    cleardevice();
 
+    int constant = 200;
+    rectangle(50 + constant, 50 + constant, 150 + constant, 120 + constant);
 
-
-   delay(5000);
-   closegraph();
-
+    delay(5000);
+    closegraph();
 }
-void scaling()
-{ int gd = DETECT,gm,left=100,top=200,right=200,bottom=200,x= 150,y=150,radius=50,degrees=0;
-   initgraph(&gd,&gm,NULL);
-   rectangle(50,50,150,120);
-   delay(500);
-   cleardevice();
-   
-   int constant = 2;
-   rectangle(50*constant,50*constant,150*constant,120*constant);
-   delay(5000);
-   closegraph();
 
+void scaling()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+
+    rectangle(50, 50, 150, 120);
+    delay(500);
+    cleardevice();
+
+    int constant = 2;
+    rectangle(50 * constant, 50 * constant, 150 * constant, 120 * constant);
+
+    delay(5000);
+    closegraph();
+}
+
+void rotation()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+
+    int x1 = 50, y1 = 50, x2 = 150, y2 = 120;
+    int x_center = (x1 + x2) / 2, y_center = (y1 + y2) / 2;
+    double angle = 45; // rotate by 45 degrees
+
+    // convert degree to radian for calculation
+    double radian = angle * M_PI / 180;
+
+    int new_x1 = x_center + (x1 - x_center) * cos(radian) - (y1 - y_center) * sin(radian);
+    int new_y1 = y_center + (x1 - x_center) * sin(radian) + (y1 - y_center) * cos(radian);
+    int new_x2 = x_center + (x2 - x_center) * cos(radian) - (y2 - y_center) * sin(radian);
+    int new_y2 = y_center + (x2 - x_center) * sin(radian) + (y2 - y_center) * cos(radian);
+
+    rectangle(new_x1, new_y1, new_x2, new_y2);
+    delay(5000);
+    closegraph();
+}
+
+void reflection()
+{
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+
+    rectangle(50, 50, 150, 120);
+    delay(1000);
+    cleardevice();
+
+    // Reflect across the x-axis
+    int y_reflect = getmaxy() - 50;
+    rectangle(50, y_reflect - 120, 150, y_reflect);
+
+    delay(5000);
+    closegraph();
 }
 
 using namespace std;
+
 int main()
 {
-    
-  
-   while (1)
-   {
-    cout<<"Choose the choice: 1) Translation 2) scaling 3) exit "<<endl;
-    int ch;
-    cin>>ch;
-
-    switch (ch)
+    while (1)
     {
-    case 1:
-        translation();
-       
-        break;
-    case 2:
-         scaling();
-      
-        break;
-    case 3:
-        goto hi;
-        break;            
-    
-    default:
-        cout<<"Please provide correct input"<<endl;
-        break;
+        cout << "Choose the choice: 1) Translation 2) Scaling 3) Rotation 4) Reflection 5) Exit" << endl;
+        int ch;
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 1:
+            translation();
+            break;
+        case 2:
+            scaling();
+            break;
+        case 3:
+            rotation();
+            break;
+        case 4:
+            reflection();
+            break;
+        case 5:
+            goto exit_loop;
+            break;
+        default:
+            cout << "Please provide correct input" << endl;
+            break;
+        }
     }
-    /* code */
-   }
 
-   hi:
-   
-
-   return 0;
+exit_loop:
+    return 0;
 }
