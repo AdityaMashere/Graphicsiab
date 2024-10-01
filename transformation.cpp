4eed99b12f1e7adfef920ecfefac3103543df849
AdityaMashere <158165635+AdityaMashere@users.noreply.github.com>
2024-10-01 09:54:48 +0530

    Update transformation.cpp

--- transformation.cpp
+++ transformation.cpp
@@ -1,90 +1,78 @@
-#include <graphics.h> 
+#include <graphics.h>
 #include <cmath>
 #include <iostream>
+#include <algorithm> // For std::min and std::max
 
-void translation()
-{
+void translation() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
 
     rectangle(50, 50, 150, 120);
     delay(2000);
-    cleardevice();
 
     int constant = 200;
     rectangle(50 + constant, 50 + constant, 150 + constant, 120 + constant);
 
-    delay(5000);
+    delay(3000);
     closegraph();
 }
 
-void scaling()
-{
+void scaling() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
-
     rectangle(50, 50, 150, 120);
     delay(500);
-    cleardevice();
 
     int constant = 2;
     rectangle(50 * constant, 50 * constant, 150 * constant, 120 * constant);
-
-    delay(5000);
+    delay(3000);
     closegraph();
 }
 
-void rotation()
-{
+void rotation() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
 
-    int x1 = 50, y1 = 50, x2 = 150, y2 = 120;
-    int x_center = (x1 + x2) / 2, y_center = (y1 + y2) / 2;
-    double angle = 45; // rotate by 45 degrees
+    int left = 50, top = 50, right = 150, bottom = 120;
 
-    // convert degree to radian for calculation
-    double radian = angle * M_PI / 180;
-
-    int new_x1 = x_center + (x1 - x_center) * cos(radian) - (y1 - y_center) * sin(radian);
-    int new_y1 = y_center + (x1 - x_center) * sin(radian) + (y1 - y_center) * cos(radian);
-    int new_x2 = x_center + (x2 - x_center) * cos(radian) - (y2 - y_center) * sin(radian);
-    int new_y2 = y_center + (x2 - x_center) * sin(radian) + (y2 - y_center) * cos(radian);
-
-    rectangle(new_x1, new_y1, new_x2, new_y2);
-    delay(5000);
-    closegraph();
-}
-
-void reflection()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, NULL);
-
-    rectangle(50, 50, 150, 120);
+    rectangle(left, top, right, bottom);
     delay(1000);
-    cleardevice();
 
-    // Reflect across the x-axis
-    int y_reflect = getmaxy() - 50;
-    rectangle(50, y_reflect - 120, 150, y_reflect);
+    float angle = 45; // Example rotation angle
+    float rad = angle * (M_PI / 180); // Convert degrees to radians
+    
+    float centerX = (left + right) / 2.0;
+    float centerY = (top + bottom) / 2.0;
 
-    delay(5000);
+    auto rotatePoint = [&](float x, float y) {
+        float newX = centerX + (x - centerX) * cos(rad) - (y - centerY) * sin(rad);
+        float newY = centerY + (x - centerX) * sin(rad) + (y - centerY) * cos(rad);
+        return std::make_pair(newX, newY);
+    };
+
+    auto p1 = rotatePoint(left, top);
+    auto p2 = rotatePoint(right, top);
+    auto p3 = rotatePoint(right, bottom);
+    auto p4 = rotatePoint(left, bottom);
+
+    line(p1.first, p1.second, p2.first, p2.second);
+    line(p2.first, p2.second, p3.first, p3.second);
+    line(p3.first, p3.second, p4.first, p4.second);
+    line(p4.first, p4.second, p1.first, p1.second);
+
+    delay(3000);
     closegraph();
 }
 
 using namespace std;
 
-int main()
-{
-    while (1)
-    {
-        cout << "Choose the choice: 1) Translation 2) Scaling 3) Rotation 4) Reflection 5) Exit" << endl;
+int main() {
+    while (1) {
+        cout << "Choose the choice: 1) Translation 2) Scaling 3) Rotation 4) Exit " << endl;
         int ch;
         cin >> ch;
 
-        switch (ch)
-        {
+        switch (ch) {
         case 1:
             translation();
             break;
@@ -95,10 +83,7 @@ int main()
             rotation();
             break;
         case 4:
-            reflection();
-            break;
-        case 5:
-            goto exit_loop;
+            goto hi;
             break;
         default:
             cout << "Please provide correct input" << endl;
@@ -106,6 +91,6 @@ int main()
         }
     }
 
-exit_loop:
+hi:
     return 0;
 }
